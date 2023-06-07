0a623635a0aaf1ad741ffe1a5da981657edd7a3c
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-08 00:03:06 +0400

    install environ

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5u@5p0(mhk!zamiv)0&#)vjo-or!yk=74-5_m0b4_5@kquiqis'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY="django-insecure-5u@5p0(mhk!zamiv)0&#)vjo-or!yk=74-5_m0b4_5@kquiqis"
 
 ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS=["http://*.127.0.0.1,localhost"]
 
 
 # Application definition
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [

85f93ca19a43c5f5395b61547474c945a881bd86
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-10 17:16:50 +0400

    created contact page

--- resume/core/urls.py
+++ resume/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,contact
 
 urlpatterns = [
     path('',index,name='index'),
+    path('contact',name='contact')
 ]

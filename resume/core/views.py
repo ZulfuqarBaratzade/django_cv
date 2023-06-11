2699276e0c6dccd718fad29544c982a009e4ad16
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-11 19:55:55 +0400

    added image

--- resume/core/views.py
+++ resume/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import General_Settings
+from core.models import General_Settings, ImageSettings
 # Create your views here.
 def index(request):
     site_title = General_Settings.objects.get(name='site_title').parameter
@@ -10,6 +10,7 @@ def index(request):
     home_banner_description = General_Settings.objects.get(name= 'home_banner_description').parameter
     about_myself_welcome = General_Settings.objects.get(name='about_myself_welcome').parameter
     about_myself_footer = General_Settings.objects.get(name='about_myself_footer').parameter
+    profile_photo = ImageSettings.objects.get(name='profile_photo').file
     context = {
         'site_title': site_title,
         'site_keywords':site_keywords,
@@ -19,8 +20,11 @@ def index(request):
         'home_banner_description':home_banner_description,
         'about_myself_welcome': about_myself_welcome,
         'about_myself_footer':about_myself_footer,
+        'profile_photo':profile_photo,
 
     }
+   
+
 
     return render(request,'index.html',context=context)
 

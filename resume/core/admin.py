edfb0b68cb720ce6eb3c6683c4679aec8994b22e
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-11 18:43:41 +0400

    add general settings

--- resume/core/admin.py
+++ resume/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from core.models import *
 # Register your models here.
+
+@admin.register(General_Settings)
+class GeneralSettingsAdmin(admin.ModelAdmin):
+    list_display = ['id','name','description','parameter','updated_date','created_date']
+    search_fields = ['id','name']
+    list_editable=['description','parameter']
+
+    class Meta:
+        model = General_Settings
+    
--- resume/core/models.py
+++ resume/core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class General_Settings(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='name',
+
+    )
+    description=models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='description',
+    )
+    parameter=models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='parameter'
+
+    )
+    updated_date=models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='update_date',
+
+    )
+    created_date=models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='created_date'
+    )
+    def __str__(self):
+        return f"General Settings: {self.name}"
+    class Meta:
+        verbose_name = "General Settings"
+        verbose_name_plural = "General Settings"
+        ordering = ('name',)

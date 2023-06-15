e82155b94962d12a44c893d1065e6278ad62e9a0
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-15 17:39:20 +0400

    created message models

--- resume/contact/admin.py
+++ resume/contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact.models import *
 
 # Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display=['id','name','email','subject','message','updated_date','created_date']
+
+    search_fields = ['name']
+
+    class Meta:
+        model = Message

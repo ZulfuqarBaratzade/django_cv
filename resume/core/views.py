1db2e702e7677eac91d8c7f3bfd5c7c01c51f59c
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-15 17:14:04 +0400

    created layout function

--- resume/core/views.py
+++ resume/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from core.models import General_Settings, ImageSettings, Skill, Experience,Education,SocialMedia,Document
 # Create your views here.
-def index(request):
+def layout(request):
     site_title = General_Settings.objects.get(name='site_title').parameter
     site_keywords = General_Settings.objects.get(name='site_keywords').parameter
     site_description = General_Settings.objects.get(name='site_description').parameter
@@ -15,6 +15,7 @@ def index(request):
     experience = Experience.objects.all()
     education = Education.objects.all()
     social_media = SocialMedia.objects.all()
+    document = Document.objects.all()
 
 
     context = {
@@ -31,44 +32,20 @@ def index(request):
         'experience':experience,
         'education':education,
         'social_media':social_media,
+        'document':document,
 
     }
-    
+    return context
+def index(request):
+    return render(request,'index.html')
 
-
-    return render(request,'index.html',context=context)
 
 def redirect_url(request,slug):
     doc = get_object_or_404(Document,slug=slug)
     return redirect(doc.file.url)
 
 def about(request):
-    site_title = General_Settings.objects.get(name='site_title').parameter
-    site_keywords = General_Settings.objects.get(name='site_keywords').parameter
-    site_description = General_Settings.objects.get(name='site_description').parameter
-    home_banner_name = General_Settings.objects.get(name='home_banner_name').parameter
-    home_banner_title = General_Settings.objects.get(name='home_banner_title').parameter
-    home_banner_description = General_Settings.objects.get(name= 'home_banner_description').parameter
-    about_myself_welcome = General_Settings.objects.get(name='about_myself_welcome').parameter
-    about_myself_footer = General_Settings.objects.get(name='about_myself_footer').parameter
-    profile_photo = ImageSettings.objects.get(name='profile_photo').file
-    skills = Skill.objects.all().order_by('order')
-
-    context = {
-        'site_title': site_title,
-        'site_keywords':site_keywords,
-        'site_description': site_description,
-        'home_banner_name':home_banner_name,
-        'home_banner_title':home_banner_title,
-        'home_banner_description':home_banner_description,
-        'about_myself_welcome': about_myself_welcome,
-        'about_myself_footer':about_myself_footer,
-        'profile_photo':profile_photo,
-        'skills': skills,
-
-    }
-    
-    return render(request,'about.html',context=context)
+    return render(request,'about.html')
 def blog(request):
     return render(request,'blog.html')
 def contact(request):
